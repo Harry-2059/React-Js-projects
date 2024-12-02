02be719c7279ff9990ed2df9b11b2d522ec5f8d0
Hari Shankar Rana <ranahari836@gmail.com>
2024-12-03 00:10:47 +0545

    commiting the project 01

--- 01begin/src/App.js
+++ 01begin/src/App.js
@@ -1,12 +1,12 @@
-
+import First from './First.js';
 
 
 function App() {
   return (
-<h1>lets begin react
-<br />
-we are creating the react project.
-</h1>
+  <>
+ 
+  <First/>
+  </>
   );
 }
 
---
+++ 01begin/src/First.js
@@ -0,0 +1,6 @@
+function First(){
+    return(
+        <h1>where is the code?</h1>
+    )
+}
+export default First;
